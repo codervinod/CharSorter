c79f9b0941649536d01c107b8c8b366923fb3ec2
Vinod Gupta <vinod_gupta@live.com>
2015-01-31 11:32:22 -0800

    Find anagrams in english dictionary

--- CharSorter/main.cpp
+++ CharSorter/main.cpp
@@ -7,9 +7,70 @@
 //
 
 #include <iostream>
+#include <string>
+class CharSorter
+{
+public:
+    CharSorter()
+    {
+        Reset();
+    }
+    virtual ~CharSorter()
+    {
+        
+    }
+
+    
+    void CountChars(std::string sentence)
+    {
+        Reset();
+        for(std::string::const_iterator itr = sentence.begin(); itr != sentence.end(); ++itr)
+        {
+            char c = *itr;
+            if(c >='A' && c <='Z')
+            {
+                c = 'a' + (c - 'A');
+            }
+            int pos=-1;
+            if( c >= 'a' && c <= 'z')
+            {
+                pos = c - 'a';
+                _chars[pos] = _chars[pos] + 1;
+            }
+            
+        }
+    }
+    
+    void PrintChars()
+    {
+        for(int i=0;i<26;++i)
+        {
+            for(int j =0;j<_chars[i];++j)
+            {
+                std::cout<<(char) ('a' + i);
+            }
+        }
+        std::cout<<std::endl;
+    }
+private:
+    void Reset()
+    {
+        for(int i=0;i<26;++i)
+        {
+            _chars[i] = 0;
+        }
+    }
+    char _chars[26];
+};
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    std::string sentence = "When not studying nuclear physics, Bambi likes to play beach volleyball.";
+    
+    CharSorter sorter;
+    sorter.CountChars(sentence);
+    sorter.PrintChars();
+    
+    //Expected "aaaaabbbbcccdeeeeeghhhiiiiklllllllmnnnnooopprsssstttuuvwyyyy"
+    //Actual   "aaaaabbbbcccdeeeeeghhhiiiiklllllllmnnnnooopprsssstttuuvwyyyy"
     return 0;
 }
